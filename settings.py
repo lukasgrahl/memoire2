ec8daa3b669b2c221d911870f19235fc60548d65
Lukas Grahl <lukas.g@amkat.de>
2024-01-22 11:06:40 +0100

    added notebooks

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) #os.path.dirname(__file__)
+
+DATA_DIR = os.path.join(PROJECT_ROOT, "data")
+GRAPHS_DIR = os.path.join(PROJECT_ROOT, 'latex', 'graphs')
+
+RANDOM_SEED = 101
