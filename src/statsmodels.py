e6ac315612e31bdda653bf67fa9a5d8b18ebeec4
Lukas Grahl <lukas.g@amkat.de>
2024-04-04 22:36:40 +0200

    added dfbetas

--- src/statsmodels.py
+++ src/statsmodels.py
@@ -122,6 +122,38 @@ def get_statsmodels_summary(lst_mods, cols_out: str = 'print', vecm_endog_index:
     return out
 
 
+def get_dfbetas(X: np.array, resid: np.array):
+    assert X.shape[0] == resid.shape[0], "X and resid do not correspond"
+    lst_dfbetas = []
+    
+    H = X @ np.linalg.inv(X.T @ X) @ X.T
+    H_diag = np.diagonal(H)
+    for idx in range(X.shape[0]):
+        x_i = X[idx]
+        e_i = resid[idx]
+        
+        lst_dfbetas.append(
+            (np.linalg.inv(X.T @ X) @ x_i[None].T @ e_i[None]) / (1 - H_diag[idx])
+        )       
+    return np.array(lst_dfbetas)    
+    
+
+def get_cooks_distance(X: np.array, resid: np.array, flt_largest_perc: float = 97.5):
+    n, p = len(resid), np.linalg.matrix_rank(X)
+    
+    s_2 = resid.T @ resid / (n - p)
+    H = X @ np.linalg.inv(X.T @ X) @ X.T
+    H_diag = np.diagonal(H)
+    
+    lst_cooks_dist = []
+    for idx in range(X.shape[0]):
+        d_i = resid[idx] ** 2 / p * s_2 * (H_diag[idx] / (1 - H_diag[idx]) ** 2)
+        lst_cooks_dist.append(d_i)
+    arr_cook_dist = np.array(lst_cooks_dist)
+    filt_percent = arr_cook_dist >= np.percentile(arr_cook_dist, flt_largest_perc)
+    return arr_cook_dist, filt_percent
+
+
 def get_fig_subplots(n_plots: int, n_cols: int = 2, figsize: tuple =(6,3.5), **kwargs):
     n_rows = int(np.ceil(n_plots/n_cols))
     fig, ax = plt.subplots(n_rows, n_cols, figsize=(figsize[0] * n_rows, figsize[1] * n_cols), **kwargs)
